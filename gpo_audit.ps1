3d2d78515ae19466e815d91e6eefb1d92431959f
Zaret-Cortorreal <zaret.belioska@gmail.com>
2026-04-15 15:43:46 -0400

    Update gpo_audit.ps1

--- gpo_audit.ps1
+++ gpo_audit.ps1
@@ -1,1 +1,56 @@
-404: Not Found
+
+Microsoft (R) Windows (R) Operating System Group Policy Result tool v2.0
+c Microsoft Corporation. All rights reserved.
+
+Created on ?4/?15/?2026 at 12:35:48 PM
+
+
+
+RSOP data for TITAN\Administrator on WIN-FHCV84NKJ3F : Logging Mode
+--------------------------------------------------------------------
+
+OS Configuration:            Primary Domain Controller
+OS Version:                  10.0.20348
+Site Name:                   N/A
+Roaming Profile:             N/A
+Local Profile:               C:\Users\Administrator
+Connected over a slow link?: No
+
+
+USER SETTINGS
+--------------
+    CN=Administrator,CN=Users,DC=titan,DC=local
+    Last time Group Policy was applied: 4/15/2026 at 12:34:18 PM
+    Group Policy was applied from:      WIN-FHCV84NKJ3F.titan.local
+    Group Policy slow link threshold:   500 kbps
+    Domain Name:                        TITAN
+    Domain Type:                        Windows 2008 or later
+    
+    Applied Group Policy Objects
+    -----------------------------
+        N/A
+
+    The following GPOs were not applied because they were filtered out
+    -------------------------------------------------------------------
+        Local Group Policy
+            Filtering:  Not Applied (Empty)
+
+    The user is a part of the following security groups
+    ---------------------------------------------------
+        Domain Users
+        Everyone
+        BUILTIN\Administrators
+        BUILTIN\Users
+        BUILTIN\Pre-Windows 2000 Compatible Access
+        NT AUTHORITY\INTERACTIVE
+        CONSOLE LOGON
+        NT AUTHORITY\Authenticated Users
+        This Organization
+        LOCAL
+        Group Policy Creator Owners
+        Domain Admins
+        Enterprise Admins
+        Schema Admins
+        Authentication authority asserted identity
+        Denied RODC Password Replication Group
+        High Mandatory Level
